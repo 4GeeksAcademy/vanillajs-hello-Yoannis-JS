911188d62b0802f0a7bb9866c3a271c08375734c
Yoan <dafneangel3008@gmail.com>
2026-01-31 10:43:48 +0000

    geerador de escusas Yoannis

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ['el perro','mi abuela','su tortuga','mi pájaro'];
+let what = ['se comió','orinó','aplastó','rompió'];
+let when = ['antes de la clase','justo a tiempo','cuando terminé','durante mi almuerzo','mientras rezaba'];
+
+function generarExcusa() {
+  let quien = who[Math.floor(Math.random() * who.length)];
+  let que = what[Math.floor(Math.random() * what.length)];
+  let cuando = when[Math.floor(Math.random() * when.length)];
+
+  let excusa = `${quien} ${que} mi tarea ${cuando}`;
+
+  return excusa.charAt(0).toUpperCase() + excusa.slice(1);
+}
+
+window.onload = () => {
+  document.getElementById("excuse").innerHTML = generarExcusa();
+
+}
+document.getElementById("btn").addEventListener("click", () => {
+  document.getElementById("excuse").innerHTML = generarExcusa();
+ });
